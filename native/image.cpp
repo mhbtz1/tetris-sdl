8587da8081335880e2a739f8d1363ccf908b063b
Matthew Habtezgi <mhbtz@mit.edu>
2024-06-06 17:03:47 -0400

    upd cpp settings vscode

--- native/image.cpp
+++ native/image.cpp
@@ -106,8 +106,15 @@ int main( int argc, char* args[] )
 			SDL_UpdateWindowSurface( gWindow );
 
             //Hack to get window to stay up
-            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
-		}
+            SDL_Event e; 
+            bool quit = false; 
+            while( quit == false ){ 
+                while( SDL_PollEvent( &e ) ){ 
+                    if( e.type == SDL_QUIT ) {
+                        quit = true; 
+                    } else if (e.type == SDL_MO)
+                }
+		    }
 	}
 
 	//Free resources and close SDL
